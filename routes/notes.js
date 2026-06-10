b51e1ab065a60dc68ed50c8a2ef1423629ee580f
tsuto-mountain <meta.mountain@gmail.com>
2026-06-10 20:46:51 +0900

    notes.jsをMongoDB対応に変更 retrieval logic
    Update MongoDB connection string and query parameters.

--- routes/notes.js
+++ routes/notes.js
@@ -1,17 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-// 接続情報を設定
 const { MongoClient } = require("mongodb");
+
 const uri = "mongodb+srv://metamountain_db_user:password@cluster0.sgeohtj.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
-// データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
 
-// idが１のドキュメントを取得
 const query = { id: 2 };
 const note = await notes.findOne(query);
 
